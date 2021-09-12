09936bd6d299a3b21344a4e3784ba2082235d0ac
Jhonatan Lyons <jhonatanlyly@gmail.com>
2021-09-12 17:33:53 -0500

    New2

--- TeindaGenerica/ScriptDataBase.sql
+++ TeindaGenerica/ScriptDataBase.sql
@@ -5,3 +5,8 @@ create table Clientes (cedula_cliente bigint not null, direccion_cliente varchar
 create table Ventas (codigo_venta bigint not null, cedula_cliente bigint not null, cedula_usuario bigint not null, ivaventa double not null, total_venta double not null, valor_venta double not null, foreign key(cedula_cliente) references Clientes (cedula_cliente));
 
 nueva linea para validar
+
+fdsfdsfds
+dsfdsf
+
+dsfsdf
